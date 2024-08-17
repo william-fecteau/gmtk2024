1cf049b6842f7177394fd21b549202226afdc5ca
Zack Dufresne <zack.dufresne.1@ulaval.ca>
2024-08-17 13:28:15 -0400

    Character renamed to card

--- states/inGameState.py
+++ states/inGameState.py
@@ -9,7 +9,7 @@ from states.payloads import InGameStatePayload
 from .state import State
 
 
-class CharacterSlot:
+class CardSlot:
     def __init__(self, x: int, y: int, size: int):
         self.surf = pygame.Surface((size, size))
         self.surf.fill((46, 46, 46))
@@ -22,20 +22,20 @@ class CharacterSlot:
     def setColor(self, color):
         self.surf.fill(color)
 
-    def characterInside(self, character):
-        if self.rect.contains(character.rect):
+    def cardInside(self, card):
+        if self.rect.contains(card.rect):
             return True
         return False
 
 
-class Character:
-    def __init__(self, character: str, x: int, y: int, size: int):
-        self.character = character
+class Card:
+    def __init__(self, card: str, x: int, y: int, size: int):
+        self.card = card
 
         self.surf = pygame.Surface((size, size))
         self.surf.fill((147, 147, 147))
 
-        text_surf = pygame.font.Font(None, 32).render(character, True, (0, 0, 0))
+        text_surf = pygame.font.Font(None, 32).render(card, True, (0, 0, 0))
         text_rect = text_surf.get_rect(center=self.surf.get_rect().center)
         self.surf.blit(text_surf, text_rect)
 
@@ -61,66 +61,66 @@ class InGameState(State):
 
         mouse_pos = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
-            if self.selected_character is None:
-                for character in self.characters:
-                    if character.rect.collidepoint(mouse_pos):
-                        self.selected_character = character
-                        self.mouse_click_offset = np.array(mouse_pos) - np.array(character.rect.topleft)
+            if self.selected_card is None:
+                for card in self.cards:
+                    if card.rect.collidepoint(mouse_pos):
+                        self.selected_card = card
+                        self.mouse_click_offset = np.array(mouse_pos) - np.array(card.rect.topleft)
                         break
         else:
-            self.selected_character = None
-            for slot in self.character_slots:
-                for character in self.characters:
-                    if slot.characterInside(character):
+            self.selected_card = None
+            for slot in self.card_slots:
+                for card in self.cards:
+                    if slot.cardInside(card):
                         slot.setColor(GREEN_COLOR)
                         break
                     else:
                         slot.setColor(DARK_GRAY)
 
-        if self.selected_character is not None:
+        if self.selected_card is not None:
             offset_pos = np.array(mouse_pos) - np.array(self.mouse_click_offset)
-            self.selected_character.move(offset_pos)  # type: ignore
+            self.selected_card.move(offset_pos)  # type: ignore
 
     def draw(self, screen) -> None:
-        for character_slot in self.character_slots:
-            character_slot.draw(screen)
+        for card_slot in self.card_slots:
+            card_slot.draw(screen)
 
-        for character in self.characters:
-            character.draw(screen)
+        for card in self.cards:
+            card.draw(screen)
 
-    def init_character_slots(self):
-        self.character_slots: list[CharacterSlot] = []
-        self.characters: list[Character] = []
+    def init_card_slots(self):
+        self.card_slots: list[CardSlot] = []
+        self.cards: list[Card] = []
 
         slot_size = 60
         slot_offset = 10
 
-        character_size = 40
-        character_offset = 10
+        card_size = 40
+        card_offset = 10
 
-        nb_characters = len(self.level.cards)
+        nb_cards = len(self.level.cards)
 
-        slot_width = nb_characters * (slot_size + slot_offset)
-        character_width = nb_characters * (character_size + character_offset)
+        slot_width = nb_cards * (slot_size + slot_offset)
+        card_width = nb_cards * (card_size + card_offset)
 
         start_slot = np.array(self.game.screen.get_rect().center) - np.array((slot_width // 2, slot_size // 2))
-        start_character = np.array(self.game.screen.get_rect().center) - \
-            np.array((character_width // 2, -character_size // 2 - 20))
+        start_card = np.array(self.game.screen.get_rect().center) - \
+            np.array((card_width // 2, -card_size // 2 - 20))
 
         for i, card in enumerate(self.level.cards):
             value = card.value
 
-            self.character_slots.append(CharacterSlot(
+            self.card_slots.append(CardSlot(
                 start_slot[0] + i * (slot_size + slot_offset), start_slot[1], slot_size))
-            self.characters.append(
-                Character(value, start_character[0] + i * (character_size + character_offset), start_character[1], character_size))
+            self.cards.append(
+                Card(value, start_card[0] + i * (card_size + card_offset), start_card[1], card_size))
 
     def onEnterState(self, payload: InGameStatePayload) -> None:
         self.level = load_level("res/levels/1.json")
 
-        self.selected_character: Character | None = None
+        self.selected_card: Card | None = None
         self.mouse_click_offset = (0, 0)
-        self.init_character_slots()
+        self.init_card_slots()
 
     def onExitState(self) -> None:
         pass
